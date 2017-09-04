1095bef2c8956c591555e00e32eba21898c4bb0c
ezequieloscarescobar <ezequieloscarescobar2@gmail.com>
2017-09-03 23:31:30 -0300

    Arrelos en hbs

--- src/main/java/controllers/InicioController.java
+++ src/main/java/controllers/InicioController.java
@@ -1,13 +1,20 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import model.SupermecadoModel;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class InicioController {
+	private Map<String, Object> model=new HashMap<>();
+	
 	public ModelAndView inicio(Request req, Response res){
 		SupermecadoModel modelSuper = new SupermecadoModel();
-		return new ModelAndView(modelSuper.getAll(), "inicio.hbs");
+		modelSuper.boot();
+		model.put("supermercados", modelSuper.getAll());
+		return new ModelAndView(model, "inicio.hbs");
 	}
 }
